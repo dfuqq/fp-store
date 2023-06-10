9a182d97c972014aeeb11eb3bbff8603e8f960ac
dfuqq <dan38449@mail.ru>
2023-06-11 00:19:01 +0500

    Sidebar Functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { SidebarContext } from '../contexts/SidebarContext';
+
+import { BsBag } from 'react-icons/bs';
 
 const Header = () => {
+	const { isOpen, setIsOpen } = useContext(SidebarContext);
 	return (
-		<div>
+		<header className='bg-pink-200'>
 			<div>Header</div>
-			<div>Open/Close sidebar</div>
-		</div>
+			<div
+				onClick={() => setIsOpen(!isOpen)}
+				className='cursor-pointer'>
+				<BsBag className='text-2xl' />
+			</div>
+		</header>
 	);
 };
 
