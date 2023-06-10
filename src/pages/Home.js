7878a00dc4ae21e89064d8bb13b96d8be5c7c5bc
dfuqq <dan38449@mail.ru>
2023-06-10 23:09:08 +0500

    Catalog a-layout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import { ProductContext } from '../contexts/ProductContext';
 
 const Home = () => {
-	return <div>Homepage</div>;
+	// Get Products from ProductContext
+	const { products } = useContext(ProductContext);
+
+	return (
+		<div>
+			<section className='py-16'>
+				<div className='container mx-auto'>
+					<div
+						className='grid grid-cols-1 md:gridcols-2 lg:grid-cols-4 
+						xl:grid-cols-5 gap-[30px] max-w-sm mx-auto 
+						md:max-w-none md:mx-0'>
+						{products.map((product) => {
+							return (
+								<div
+									className='w-full h-[300px] bg-pink-200 mb-4'
+									key={product.id}>
+									{product.title}
+								</div>
+							);
+						})}
+					</div>
+				</div>
+			</section>
+		</div>
+	);
 };
 
 export default Home;
