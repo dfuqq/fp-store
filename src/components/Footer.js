62435cec1bf5464162ca99fdd556c18ccb1c8786
dfuqq <dan38449@mail.ru>
2023-06-12 00:04:32 +0500

    Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const Footer = () => {
-	return <div>Footer</div>;
+	return (
+		<footer className='bg-primary py-12'>
+			<div className='container mx-auto'>
+				<p className='text-white text-center'>
+					Final Project for UrFU TS Inside. 2023.
+				</p>
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;
