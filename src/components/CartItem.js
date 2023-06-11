82a3aa0f96afe38aa923b0e807fba99d33c1292f
dfuqq <dan38449@mail.ru>
2023-06-11 22:20:25 +0500

    UI

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
 import { IoMdClose, IoMdAdd, IoMdRemove } from 'react-icons/io';
 
+import { CartContext } from '../contexts/CartContext';
+
 const CartItem = ({ item }) => {
+	const { removeFromCart } = useContext(CartContext);
 	// Destructure Item
 	const { id, title, image, price, amount } = item;
 
 	return (
-		<div className='flex'>
+		<div
+			className='flex gap-x-4 py-2 lg:px-6 border-b border-gray-200
+        w-full font-light text-gray-500'>
 			<div
 				className='w-full min-h-[150px] flex items-center 
             gap-x-4'>
@@ -32,7 +37,9 @@ const CartItem = ({ item }) => {
 						</Link>
 
 						{/* {Remove Icon} */}
-						<div className='text-xl cursor-pointer'>
+						<div
+							onClick={() => removeFromCart(id)}
+							className='text-xl cursor-pointer'>
 							<IoMdClose
 								className='text-gray-500 hover:text-red-500 
                         transition'
@@ -66,9 +73,16 @@ const CartItem = ({ item }) => {
 							</div>
 						</div>
 						{/* {Item Price} */}
-						<div>item price</div>
-						{/* {Final Price} */}
-						<div>Final Price</div>
+						<div className='flex-2 flex items-center justify-around'>
+							${price} <br />
+							per item
+						</div>
+						{/* {Final Price to 2 decimals} */}
+						<div
+							className='flex-1 flex justify-end items-center 
+                        text-primary font-medium'>{`$${parseFloat(
+							price * amount
+						).toFixed(2)}`}</div>
 					</div>
 				</div>
 			</div>
